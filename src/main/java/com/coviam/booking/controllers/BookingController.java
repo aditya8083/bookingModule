ccd5f57c6718742157f56c97a5aed3cd80e127fa
aditya8083 <apandey8083@gmail.com>
2018-02-12 20:09:24 +0530

    api changes commit

--- src/main/java/com/coviam/booking/controllers/BookingController.java
+++ src/main/java/com/coviam/booking/controllers/BookingController.java
@@ -8,16 +8,14 @@ import com.coviam.booking.service.BookingService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
+@CrossOrigin("*")
+@RequestMapping("/booking")
 public class BookingController {
 
     @Autowired
